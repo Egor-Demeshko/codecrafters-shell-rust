b1c732b77c2a69c5d5bf01ee14afc8ab23f29440
Egor Demeshko <demeshko.egor@gmail.com>
2026-01-04 12:37:29 +0300

    fix: command not found message

--- src/commands/type_command.rs
+++ src/commands/type_command.rs
@@ -1,10 +1,6 @@
 use crate::helpers::output;
 
-pub fn execute(
-    argv: Vec<String>,
-    command_list: Vec<&str>,
-    command_not_found: fn(text: &str) -> (),
-) -> () {
+pub fn execute(argv: Vec<String>, command_list: Vec<&str>) -> () {
     if argv[1].is_empty() {
         println!("provide command name to search for, ex. <type exit>");
         return;
@@ -12,6 +8,6 @@ pub fn execute(
     let command_name = argv[1].as_str();
     match command_list.iter().find(|value| **value == command_name) {
         Some(command) => output(format!("{command} is a shell builtin\n").as_str()),
-        None => command_not_found(command_name),
+        None => println!("{command_name}: not found"),
     }
 }

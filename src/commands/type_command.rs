4880f432815b4f8934796fd81ba0216bf097f0f4
Egor Demeshko <demeshko.egor@gmail.com>
2026-01-07 14:30:15 +0300

    fix: try to resolve exact place were PATH should be found

--- src/commands/type_command.rs
+++ src/commands/type_command.rs
@@ -3,6 +3,7 @@ use std::{
     env,
     fs::{self},
     os::unix::fs::PermissionsExt,
+    path::Path,
 };
 
 pub fn execute(argv: Vec<String>, command_list: Vec<&str>) -> () {
@@ -34,29 +35,57 @@ pub fn execute(argv: Vec<String>, command_list: Vec<&str>) -> () {
 fn search_in_path(command: &str) -> Option<String> {
     match env::var("PATH") {
         Ok(routes) => {
+            let mut shortest_path = String::new();
             for route in env::split_paths(&routes) {
-                let dir = route.as_path();
-                if let Ok(entries) = fs::read_dir(dir) {
-                    for entry in entries.flatten() {
-                        let file_name = entry.file_name();
-                        if file_name.to_string_lossy().contains(command) {
-                            let file = fs::metadata(entry.path()).unwrap();
-                            let permissions = file.permissions();
-                            if permissions.mode() & 0o111 == 0 {
-                                continue;
-                            }
-                            return Some(format!(
-                                "{} is {}/{}\n",
-                                command,
-                                dir.to_str().unwrap(),
-                                command
-                            ));
-                        }
-                    }
+                if route.as_os_str().is_empty() {
+                    continue;
+                }
+                let text = match search_path(route.as_path(), command) {
+                    Some(text) => text,
+                    None => continue,
+                };
+                if text.is_empty() {
+                    continue;
+                }
+                if shortest_path.is_empty() || shortest_path.len() > text.len() {
+                    shortest_path = text;
                 }
             }
-            None
+
+            if shortest_path.is_empty() {
+                return None;
+            }
+            Some(format!("{} is {}\n", command, shortest_path))
         }
         Err(_) => None,
     }
+}
+
+fn search_path(dir: &Path, command: &str) -> Option<String> {
+    let address = dir.join(command);
+    let full_address = address.as_path();
+    if !full_address.is_file() {
+        return None;
+    }
+
+    if is_executable(full_address) {
+        return Some(format!(
+            "{}",
+            full_address.as_os_str().to_str().unwrap_or("")
+        ));
+    }
+    None
+}
+
+fn is_executable(path: &Path) -> bool {
+    let metadata_res = fs::metadata(path);
+    if metadata_res.is_err() {
+        return false;
+    }
+
+    let metadata = metadata_res.unwrap();
+    if metadata.permissions().mode() & 0o111 != 0 {
+        return true;
+    }
+    return false;
 }

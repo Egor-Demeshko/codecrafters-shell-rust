130de415378ad596001ed8bd554e5628c650a324
Egor Demeshko <demeshko.egor@gmail.com>
2026-01-25 18:15:06 +0300

    fix: pwd command prints output with \n at the end

--- src/commands/pwd_command.rs
+++ src/commands/pwd_command.rs
@@ -4,7 +4,7 @@ use crate::domains::execute_command::ExecuteOptions;
 
 pub fn execute(options: &ExecuteOptions) -> () {
     match env::current_dir() {
-        Ok(path) => options.output(format!("{}", path.display()).as_str()),
+        Ok(path) => options.output(format!("{}\n", path.display()).as_str()),
         Err(e) => eprintln!("{}", e),
     }
 }
